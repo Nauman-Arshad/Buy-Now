349418c9b4e67604d3681e17b95862b658f10abf
Nauman-Arshad <naumankhan642@gmail.com>
2021-11-05 20:49:51 +0500

    add style and products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :products
   resources :posts
   devise_for :users
   resources :pages
-  root to: 'pages#index'
+  root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
